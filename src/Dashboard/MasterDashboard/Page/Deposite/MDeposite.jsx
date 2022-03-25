4b54c7faee7f8a8e6ecdfab43840c4e9fc3de627
Sai <sai081118@gmail.comgit commit>
2022-03-25 16:58:26 +0630

    Filter

--- src/Dashboard/MasterDashboard/Page/Deposite/MDeposite.jsx
+++ src/Dashboard/MasterDashboard/Page/Deposite/MDeposite.jsx
@@ -1,9 +1,11 @@
 
 
 import { useState } from "react";
+import { ToastContainer } from "react-toastify";
 import MDepositeTable from "../../../../components/Table/MDepostie/MDepositeTable";
 import Card from "../../../../components/UI/Card";
-import classes from "./MDeposite.module.css"
+import 'react-toastify/dist/ReactToastify.css';
+import classes from "./MDeposite.module.css";
 
 const MDeposite = ()=>{
 const [num,setNum] = useState(0);
@@ -11,6 +13,7 @@ const [num,setNum] = useState(0);
 return(
     <div>
         <Card>
+            <ToastContainer />
      <div className={classes["card-header"]}>
           <h1 className={classes["card-title"]}>Deposite</h1>
      </div>
