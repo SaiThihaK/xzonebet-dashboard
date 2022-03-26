09215b31a57f2478ec4f381fb14fcfad20668aef
Sai <sai081118@gmail.comgit commit>
2022-03-26 14:59:12 +0630

    newBranch

--- src/Dashboard/MasterDashboard/Page/TransitionHistory/TransitionHistoryTable/TransitionHistoryTable.jsx
+++ src/Dashboard/MasterDashboard/Page/TransitionHistory/TransitionHistoryTable/TransitionHistoryTable.jsx
@@ -11,6 +11,11 @@ import Stack from "@mui/material/Stack";
 import classes from "./TransitionHistoryTable.module.css";
 import { Button } from "@mui/material";
 import { BasedColor } from "../../../../../Controller/BasedColor";
+import { ToastContainer,toast } from "react-toastify";
+import axios from "axios";
+import { getMethod } from "../../../../../services/api-services";
+import CustomPagination from "../../../../../components/Pagination/CustomPagination";
+import { ChangeDate } from "../../../../../Controller/ChangeDate";
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -22,31 +27,63 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     fontSize: 14,
   },
 }));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 const TransitionHistoryTable = () => {
   
-
+  
 
   const filterArr = [23,"HellFire","Yolo",23,45,"reiyrieuy"];
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    "&:last-child td, &:last-child th": {
-      border: 0,
-    },
-  }));
  
+ 
+  
+    const [page,setPage] = React.useState(1);
+    const [totalPage,setTotalPage]  = React.useState(0);
+    const [transferData,setTransferData] = React.useState([]);
+
+  const getTransitionData = async()=>{
+    try{
+      const response = await axios.request(getMethod(`api/wallet/transfer-record?sortColumn=id&sortDirection=desc&limit=10&page=${page}`));
+      console.log(response.data.data);
+      setTotalPage(response.data.meta.last_page);
+      setTransferData(response.data.data);
+    }catch(error){
+      console.log(error);
+    }
+
+ 
+  }
+
+  useEffect(()=>{
+    getTransitionData();
+    return ()=>{
+      setTransferData([])
+    }
+  },[page])
+  
+ const AlertToast = msg =>msg;
+
+
   return (
     <div className={classes["table-margin"]}>
+
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
           <TableHead>
             <TableRow>
               <StyledTableCell>No.</StyledTableCell>
+              <StyledTableCell align="right">Id</StyledTableCell>
               <StyledTableCell align="right">Date</StyledTableCell>
-              <StyledTableCell align="right">Payment Type</StyledTableCell>
-              <StyledTableCell align="right">Payment Provider</StyledTableCell>
+              <StyledTableCell align="right">Transfer Amount</StyledTableCell>
+         
               <StyledTableCell align="right">Ammount</StyledTableCell>
               <StyledTableCell align="right">
                 From
@@ -56,17 +93,18 @@ const TransitionHistoryTable = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {filterArr.map((row, index) => (
+            {transferData.map((row, index) => (
               <StyledTableRow key={index}>
                 <StyledTableCell component="th" scope="row">
                   {index+1}
                 </StyledTableCell>
-                <StyledTableCell align="right">{row[0]}</StyledTableCell>
-                <StyledTableCell align="right">{row[1]}</StyledTableCell>
-                <StyledTableCell align="right">{row[2]}</StyledTableCell>
-                <StyledTableCell align="right">{row[3]}</StyledTableCell>
-                <StyledTableCell align="right">{row[3]}</StyledTableCell>
-                <StyledTableCell align="right">{row[3]}</StyledTableCell>
+                <StyledTableCell align="right">{row.id}</StyledTableCell>
+                <StyledTableCell align="right">{ChangeDate(row.created_at)}</StyledTableCell>
+                <StyledTableCell align="right">{row.transfer_amount}</StyledTableCell>
+                <StyledTableCell align="right">--</StyledTableCell>
+                <StyledTableCell align="right">{row.sender_user_id}</StyledTableCell>
+               
+                <StyledTableCell align="right">{row.receiver_user_id}</StyledTableCell>
                 <StyledTableCell align="right">
                   <Button  size="small" variant="contained">
                   With Draw
@@ -77,6 +115,7 @@ const TransitionHistoryTable = () => {
           </TableBody>
         </Table>
       </TableContainer>
+      <CustomPagination totalPage={totalPage} setPage={setPage} />
     </div>
   );
 };
