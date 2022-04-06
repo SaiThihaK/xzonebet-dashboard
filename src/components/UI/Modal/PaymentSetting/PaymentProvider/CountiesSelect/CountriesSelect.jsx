d21ac99bbf2b460e46621df3df61ea7a7040ceb6
Sai <sai081118@gmail.comgit commit>
2022-04-06 11:22:14 +0630

    dev fix-selectCountries

--- src/components/UI/Modal/PaymentSetting/PaymentProvider/CountiesSelect/CountriesSelect.jsx
+++ src/components/UI/Modal/PaymentSetting/PaymentProvider/CountiesSelect/CountriesSelect.jsx
@@ -1,6 +1,4 @@
-import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
-import "primereact/resources/primereact.min.css";                  //core css
-import "primeicons/primeicons.css";                                //icons
+
 import { Countries } from "../../../../../../services/api-services";
 
 import { useEffect, useState } from "react";
