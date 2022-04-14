a6e4de0116232c262ef0d61255171b5f1eb1c12b
Sai <sai081118@gmail.comgit commit>
2022-04-14 11:27:22 +0630

    new bet modal

--- src/components/UI/Modal/Newbet/NewbetModal.jsx
+++ src/components/UI/Modal/Newbet/NewbetModal.jsx
@@ -3,21 +3,19 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import classes from "./NewbetModal.module.css"
-import { Button, Grid, MenuItem, Select } from "@mui/material";
+import { Button, Grid, MenuItem, Select ,Stack} from "@mui/material";
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import TimePicker from '@mui/lab/TimePicker';
+
 import DateTimePicker from '@mui/lab/DateTimePicker';
-import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
-import MobileDatePicker from '@mui/lab/MobileDatePicker';
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "60%",
   transform: "translate(-50%, -50%)",
-  width: 700,
+  width: 900,
   bgcolor: "background.paper",
   border: "1px solid #FFF",
   borderRadius: "0.5rem",
@@ -25,7 +23,7 @@ const style = {
 };
 
 const NewbetModal = ({ open, closeHandler,}) => {
-    const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
+    const [value, setValue] = React.useState(new Date('2022-08-18T21:11:54'));
 
     const handleChange = (newValue) => {
       setValue(newValue);
@@ -45,12 +43,15 @@ const NewbetModal = ({ open, closeHandler,}) => {
           >
             New bet
           </Typography>
-          <div className={classes["container"]}>
+          <Grid container spacing={1}>
+            <Grid item xs={6}>
+            <div className={classes["container"]}>
         <label>Match Title</label>
-        <TextField size="small" style={{width:"100%"}} />
+        <p>Nigma and Og pwal</p>
           </div>
-{/* ------------------------Date and time------------------------------------------------ */}
-        <div className={classes["container"]}>
+            </Grid>
+            <Grid item xs={6}>
+            <div className={classes["container"]}>
         <label>Date and Time</label>
         <LocalizationProvider dateAdapter={AdapterDateFns}>
         <DateTimePicker
@@ -62,13 +63,19 @@ const NewbetModal = ({ open, closeHandler,}) => {
         />
         </LocalizationProvider>
           </div>
+            </Grid>
+          </Grid>
+         
+
+       
 {/* ===================Team 1================================================== */}
-         <Grid container spacing={3} style={{marginTop:20}}>
+<hr style={{margin:"20px 0px"}} />
+         <Grid container spacing={3}>
    <Grid item xs={2}>
    <label>Team 1</label>
    </Grid>
    <Grid item xs={6}>
-   <TextField size="small" style={{width:"100%"}} />
+   <p>TEAM OG</p>
    </Grid>
    <Grid item xs={4}>
    <Select size="small" style={{width:"100%"}}>
@@ -78,13 +85,30 @@ const NewbetModal = ({ open, closeHandler,}) => {
         </Grid>
          </Grid>
 
+
+    <Grid container spacing={1}>
+      <Grid item xs={6} style={{marginTop:10}}>
+    <div className={classes["team-container"]}> 
+   <label>Country</label>
+   <p>UAE</p>
+    </div>
+      </Grid>
+      <Grid item xs={6} style={{marginTop:10}}>
+    <div className={classes["team-container"]}> 
+   <label>League</label>
+   <p>DPC Major</p>
+    </div>
+      </Grid>
+    </Grid>
+    <hr style={{margin:"20px 0px"}} />
          {/* ----------------Team 2------------------- */}
-         <Grid container spacing={3} style={{marginTop:20}}>
+      
+         <Grid container spacing={3}>
    <Grid item xs={2}>
    <label>Team 2</label>
    </Grid>
    <Grid item xs={6}>
-   <TextField size="small" style={{width:"100%"}} />
+  <p>Team Nigma Glaxy</p>
    </Grid>
    <Grid item xs={4}>
    <Select size="small" style={{width:"100%"}}>
@@ -92,20 +116,75 @@ const NewbetModal = ({ open, closeHandler,}) => {
             <MenuItem>Under</MenuItem>
         </Select>
         </Grid>
-         </Grid>
+         </Grid> 
+      <Grid container spacing={1} >
+      <Grid item xs={6} style={{marginTop:10}}>
+    <div className={classes["team-container"]}> 
+   <label>Country</label>
+   <p>Danemad</p>
+    </div>
+      </Grid>
+      <Grid item xs={6} style={{marginTop:10}}>
+    <div className={classes["team-container"]}> 
+   <label>League</label>
+   <p>DPC Major</p>
+    </div>
+      </Grid>
+    </Grid>
+    <hr style={{marginTop:20}} />
+
          {/* ---------------Body------------ */}
-         <div className={classes["container"]}>
+         <Grid container spacing={3}>
+           <Grid item xs={4}>
+           <div className={classes["container"]}>
         <label>Body</label>
-        <TextField size="small" style={{width:"100%"}} />
+        <TextField size="small" type="number" style={{width:"100%"}} />
           </div>
+           </Grid>
+           <Grid item xs={4}>
+           <div className={classes["container"]}>
+            <Select size="small" style={{width:150}}>
+              <MenuItem style={{display:"flex",justifyContent:"center",alignItem:"center",fontSize:"18px"}}>-</MenuItem>
+              <MenuItem  style={{display:"flex",justifyContent:"center",alignItem:"center",fontSize:"18px"}}>=</MenuItem>
+              <MenuItem  style={{display:"flex",justifyContent:"center",alignItem:"center",fontSize:"18px"}}>+</MenuItem>
+            </Select>
+            </div>
+           </Grid>
+           <Grid item xs={4}>
+             <div className={classes["container"]}>
+             <TextField size="small" type="number" style={{width:"100%"}} />
+             </div>
+          
+           </Grid>
+         </Grid>
+         <hr style={{marginTop:20}} />
           {/* -------------------------Total------------------------------ */}
-          <div className={classes["container"]}>
-        <label>Total</label>
-        <TextField size="small" style={{width:"100%"}} />
+          <Grid container spacing={3}>
+           <Grid item xs={4}>
+           <div className={classes["container"]}>
+        <label>Body</label>
+        <TextField size="small" type="number" style={{width:"100%"}} />
           </div>
-          <div className={classes["btn-container"]}>
-         <Button variant="contained">Save</Button>
-          </div>
+           </Grid>
+           <Grid item xs={4}>
+           <div className={classes["container"]}>
+            <Select size="small" style={{width:150}}>
+              <MenuItem style={{display:"flex",justifyContent:"center",alignItem:"center",fontSize:"18px"}}>-</MenuItem>
+              <MenuItem  style={{display:"flex",justifyContent:"center",alignItem:"center",fontSize:"18px"}}>=</MenuItem>
+              <MenuItem  style={{display:"flex",justifyContent:"center",alignItem:"center",fontSize:"18px"}}>+</MenuItem>
+            </Select>
+            </div>
+           </Grid>
+           <Grid item xs={4}>
+             <div className={classes["container"]}>
+             <TextField size="small" type="number" style={{width:"100%"}} />
+             </div>
+          
+           </Grid>
+         </Grid>
+         <div className={classes["btn-container"]}>
+          <Button variant="contained" size="large">Save</Button>
+         </div>
         </Box>
       </Modal>
     </div>
