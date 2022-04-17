5828fccb2e793dae5994afc247f1e8b92495163e
LAPTOP-FUU6KJCJ\ASUS <heinh9540@gmail.com>
2022-04-17 15:35:02 +0630

    add bet

--- src/pages/Fixture/FixtureTable/FixtureTable.jsx
+++ src/pages/Fixture/FixtureTable/FixtureTable.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -10,15 +10,12 @@ import Paper from "@mui/material/Paper";
 import classes from "./FixtureTable.module.css";
 import { BasedColor } from "../../../Controller/BasedColor";
 import { Button } from "@mui/material";
-import NewbetModal from "../../../components/UI/Modal/Newbet/NewbetModal"
+import NewbetModal from "../../../components/UI/Modal/Newbet/NewbetModal";
 import { ChangeDate } from "../../../Controller/ChangeDate";
-
-
-
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor:BasedColor.tableHead,
+    backgroundColor: BasedColor.tableHead,
     color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
@@ -35,16 +32,17 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
     border: 0,
   },
 }));
-const FixtureTable = ({bettingData}) => {
-  
- console.log(bettingData);
-  const [open,setOpen] = useState(false);
-  const [id,setId] = useState("");
-  const openHandler = (id)=>{setOpen(true)
-  setId(id);
-  }
+const FixtureTable = ({ bettingData }) => {
+  console.log(bettingData);
 
-  const closeHandler = ()=>setOpen(false);
+  const [id, setId] = useState("");
+  const [open, setOpen] = useState(false);
+  const openHandler = (id) => {
+    setOpen(true);
+    setId(id);
+  };
+
+  const closeHandler = () => setOpen(false);
   return (
     <div className={classes["table-margin"]}>
       <TableContainer component={Paper}>
@@ -56,38 +54,59 @@ const FixtureTable = ({bettingData}) => {
               {/* <StyledTableCell align="right">Time</StyledTableCell> */}
               <StyledTableCell align="right">Country</StyledTableCell>
               <StyledTableCell align="right">League</StyledTableCell>
-              <StyledTableCell align="right" >Team 1</StyledTableCell>
-              <StyledTableCell align="right" >Team 2</StyledTableCell>
+              <StyledTableCell align="right">Team 1</StyledTableCell>
+              <StyledTableCell align="right">Team 2</StyledTableCell>
               <StyledTableCell align="right"></StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-                    
-                   {bettingData.map((el,key)=>{
-                    return(  <StyledTableRow key={key}>
-                      <StyledTableCell align="right">{key+1}</StyledTableCell>
-                      <StyledTableCell align="right">{ChangeDate(el?.fixture?.date)}</StyledTableCell>
-                      <StyledTableCell align="right">{el?.league?.country}</StyledTableCell>
-                      <StyledTableCell align="right">{el?.league?.name}</StyledTableCell>
-                      <StyledTableCell align="right">{el?.teams?.home?.name}</StyledTableCell>
-                      <StyledTableCell align="right">{el?.teams?.away?.name}</StyledTableCell>
-                      <StyledTableCell align="right" sx={{width: "200px"}}>
-                 <Button onClick={()=>{openHandler(key)}}  variant="contained">New Bet</Button>
-                </StyledTableCell>
-                      </StyledTableRow>
-                   )})}  
-                {/* <StyledTableCell align="right">eeew</StyledTableCell>
+            {bettingData.map((el, key) => {
+              return (
+                <StyledTableRow key={key}>
+                  <StyledTableCell align="right">{key + 1}</StyledTableCell>
+                  <StyledTableCell align="right">
+                    {ChangeDate(el?.fixture?.date)}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {el?.league?.country}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {el?.league?.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {el?.teams?.home?.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {el?.teams?.away?.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="right" sx={{ width: "200px" }}>
+                    <Button
+                      onClick={() => {
+                        openHandler(key);
+                      }}
+                      variant="contained"
+                    >
+                      New Bet
+                    </Button>
+                  </StyledTableCell>
+                </StyledTableRow>
+              );
+            })}
+            {/* <StyledTableCell align="right">eeew</StyledTableCell>
                 <StyledTableCell align="right">eee</StyledTableCell>
                 <StyledTableCell align="right">eee</StyledTableCell>
                 <StyledTableCell align="right">www</StyledTableCell>
                 <StyledTableCell align="right">
                  <Button onClick={openHandler} variant="contained">New Bet</Button>
                 </StyledTableCell> */}
-      
           </TableBody>
         </Table>
       </TableContainer>
-      <NewbetModal open={open} bettingData={bettingData[id]} closeHandler={closeHandler} />
+      <NewbetModal
+        open={open}
+        bettingData={bettingData[id]}
+        closeHandler={closeHandler}
+      />
       {/* <CustomPagination totalPage={pagination?.meta?.last_page} setPage={setPage} /> */}
     </div>
   );
--- src/pages/OpeningBet/OpenningBet.jsx
+++ src/pages/OpeningBet/OpenningBet.jsx
@@ -1,64 +1,96 @@
-
-import React from 'react'
-import PageTitleCard from "../../components/UI/PageTitleCard/PageTitleCard"
+import React, { useState } from "react";
+import PageTitleCard from "../../components/UI/PageTitleCard/PageTitleCard";
 import classes from "./Openningbet.module.css";
 
-import { Avatar, Button, Stack } from '@mui/material';
-import {ArrowUpward,ArrowDownward} from '@mui/icons-material';
+import { Avatar, Button, Stack } from "@mui/material";
+import { ArrowUpward, ArrowDownward } from "@mui/icons-material";
+import NewbetModal from "../../components/UI/Modal/Newbet/NewbetModal";
 const OpenningBet = () => {
-    const array = ["1","2","3","4","5","6","7","8","9"];
+  const [open, setOpen] = useState(false);
+  const openHandler = () => {
+    setOpen(true);
+  };
 
+  const closeHandler = () => setOpen(false);
+  const array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   return (
     <PageTitleCard title="Opening Bet">
-        <div className={classes["card-body"]}>
-     <Stack direction="row" spacing={7}>
-         <div  style={{display:"flex"}} >
-         <Avatar size="small" style={{marginRight:10}}   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdxKj5LbvxwB1MRqLK74MCI6OCOHbOHutG1A&usqp=CAU" alt='country' />
-         <span style={{fontSize:16,marginTop:5}}>England</span>
-         </div>
-         <div style={{display:"flex"}} >
-         <Avatar size="small" style={{marginRight:10}}   src="https://assets.turbologo.com/blog/en/2020/01/19084653/Premier-League-symbol.png" alt='country' />
-         <span style={{fontSize:16,marginTop:5}}>Premier League</span>
-         </div>
-     </Stack>
-     <table className={classes["table-container"]}>
-         <tr>
-             <th></th>
-             <th></th>
-             <th>H</th>
-             <th>A</th>
-             <th>O</th>
-             <th>Total</th>
-             <th>U</th>
-             <th></th>
-         </tr>
-      
-    {
-        array.map((bet,index)=>(
+      <div className={classes["card-body"]}>
+        <Stack direction="row" spacing={7}>
+          <div style={{ display: "flex" }}>
+            <Avatar
+              size="small"
+              style={{ marginRight: 10 }}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdxKj5LbvxwB1MRqLK74MCI6OCOHbOHutG1A&usqp=CAU"
+              alt="country"
+            />
+            <span style={{ fontSize: 16, marginTop: 5 }}>England</span>
+          </div>
+          <div style={{ display: "flex" }}>
+            <Avatar
+              size="small"
+              style={{ marginRight: 10 }}
+              src="https://assets.turbologo.com/blog/en/2020/01/19084653/Premier-League-symbol.png"
+              alt="country"
+            />
+            <span style={{ fontSize: 16, marginTop: 5 }}>Premier League</span>
+          </div>
+        </Stack>
+        <table className={classes["table-container"]}>
+          <tr>
+            <th></th>
+            <th></th>
+            <th>H</th>
+            <th>A</th>
+            <th>O</th>
+            <th>Total</th>
+            <th>U</th>
+            <th></th>
+          </tr>
+
+          {array.map((bet, index) => (
             <tr key={index} className={classes["body-row"]}>
-            <td>
+              <td>
                 <p>22-4-2022</p>
                 <p>02:33:56</p>
-            </td>
-            <td className={classes["teams"]}>
-            <p className={classes["t-1"]}>Team OG<ArrowUpward style={{fontSize:14,color:"green"}} /></p>
-            <p className={classes["t-2"]}>Team Nigma galaxy<ArrowDownward style={{fontSize:14,color:"red"}} /></p>
-            </td>
-           <td className={classes["bet-amount"]} align='center'><span>3.4</span></td>
-           <td className={classes["bet-amount"]} align='center'><span>3.4</span></td>
-           <td className={classes["bet-amount"]} align='center'><span>3.4</span></td>
-           <td className={classes["bet-amount"]} align='center'><span>3.4</span></td>
-           <td className={classes["bet-amount"]} align='center'><span>3.4</span></td>
-           <td  align='center'><Button size="small" variant="contained">Edit</Button></td>
-         </tr>
-        
-        ))
-    }
-   
-     </table>
-        </div>
+              </td>
+              <td className={classes["teams"]}>
+                <p className={classes["t-1"]}>
+                  Team OG
+                  <ArrowUpward style={{ fontSize: 14, color: "green" }} />
+                </p>
+                <p className={classes["t-2"]}>
+                  Team Nigma galaxy
+                  <ArrowDownward style={{ fontSize: 14, color: "red" }} />
+                </p>
+              </td>
+              <td className={classes["bet-amount"]} align="center">
+                <span>3.4</span>
+              </td>
+              <td className={classes["bet-amount"]} align="center">
+                <span>3.4</span>
+              </td>
+              <td className={classes["bet-amount"]} align="center">
+                <span>3.4</span>
+              </td>
+              <td className={classes["bet-amount"]} align="center">
+                <span>3.4</span>
+              </td>
+              <td className={classes["bet-amount"]} align="center">
+                <span>3.4</span>
+              </td>
+              <td align="center">
+                <Button onClick={openHandler} size="small" variant="contained">
+                  Edit
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+      <NewbetModal open={open} closeHandler={closeHandler} />
     </PageTitleCard>
-  )
-}
+  );
+};
 
-export default OpenningBet
+export default OpenningBet;
