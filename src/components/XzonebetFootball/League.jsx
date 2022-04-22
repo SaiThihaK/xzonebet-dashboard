98dae946e6e9a1186fb3e138de922dc7420ac101
Sai <sai081118@gmail.comgit commit>
2022-04-22 15:05:07 +0630

    autoComplete filter error done

--- src/components/XzonebetFootball/League.jsx
+++ src/components/XzonebetFootball/League.jsx
@@ -2,6 +2,7 @@ import { FormControl, MenuItem, Select } from '@mui/material'
 import React from 'react'
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import { Box } from '@mui/system';
 
 const League = ({league,leagueIdHandler}) => {
   let data=[];
@@ -9,22 +10,19 @@ const League = ({league,leagueIdHandler}) => {
   const leagueData=league.map(el=>{
     data=[...data,el.league];
   }) 
-  const top100Films = [
-    { label: 'The Shawshank Redemption', year: 1994 },
-    { label: 'The Godfather', year: 1972 },
-    { label: 'The Godfather: Part II', year: 1974 },
-    { label: 'The Dark Knight', year: 2008 },
-    { label: '12 Angry Men', year: 1957 },
-    { label: "Schindler's List", year: 1993 },
-    { label: 'Pulp Fiction', year: 1994 }, ]
+
   return (
 <Autocomplete
   disablePortal
   onChange={(event, value) => leagueIdHandler(value.id)} 
   id="combo-box-demo"
   options={data}
-  getOptionLabel={(option) => option.name}
-
+  getOptionLabel={(option) =>option.name}
+  renderOption={(props, option) => (
+    <Box component="li" {...props} key={option.id}>
+      {option.name}
+    </Box>
+  )}
   sx={{ width: 300 }}
   renderInput={(params) => <TextField size='small' {...params} label="Select League" />}
 />
