9adcdf2f9b333823783a3b6678617391e93ab930
Sai <sai081118@gmail.comgit commit>
2022-04-23 11:39:16 +0630

    working

--- src/pages/OpeningBet/OpenningBet.jsx
+++ src/pages/OpeningBet/OpenningBet.jsx
@@ -17,7 +17,7 @@ const OpenningBet = () => {
     setOpen(true);
   };
   const closeHandler = () => setOpen(false);
-  const array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
   return (
     <PageTitleCard title="Opening Bet">
       <div className={classes["card-body"]}>
