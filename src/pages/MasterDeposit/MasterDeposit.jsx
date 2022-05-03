69a9678b67131771a379b02303f12ecfbdea3c22
Sai <sai081118@gmail.comgit commit>
2022-05-03 16:48:24 +0630

    deposite and all sport data table

--- src/pages/MasterDeposit/MasterDeposit.jsx
+++ src/pages/MasterDeposit/MasterDeposit.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-
-import MasterDepositTable from "./MasterDepositTable/MasterDepositTable";
 import classes from "./MasterDeposit.module.css";
 import Card from "../../components/UI/Card";
+import MasterDepositeDataTable from "./MasterDepositeDataTable/MasterDepositeDataTable";
+import CustomLoading from "../../components/CustomLoading/CustomLoading";
+import CustomGetFunction from "../../services/CustomGetFunction";
 const MasterDeposit = () => {
   let searchLists = [
     { label: "Master" },
     { label: "Agent" },
     { label: "User" },
   ];
+  const [num,setNum] = useState(0);
+  const {data,loading} = CustomGetFunction(`api/affiliate-register-lists-detail?sortColumn=updated_at&sortDirection=desc&limit=10&status=deposit-pending`,[num]);
   const [filterId,setFilterId] = useState("");
   return (
     <div className={classes["soccer-setting-container"]}>
@@ -46,8 +49,10 @@ const MasterDeposit = () => {
                 />
               </div>
             </div>
-
-            <MasterDepositTable filterId={filterId} />
+             {
+               loading ? (<MasterDepositeDataTable data={data} num={num} setNum={setNum} />):<CustomLoading />
+             }
+            
           </div>
         </div>
       </Card>
---
+++ src/pages/MasterDeposit/MasterDepositeDataTable/MasterDepositeDataTable.jsx
@@ -0,0 +1,121 @@
+import React,{useState} from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { ChangeDate } from '../../../Controller/ChangeDate';
+import { Button, Stack } from '@mui/material';
+import { toast } from 'react-toastify';
+import { logoutHandler } from '../../../components/Sidebar/Sidebar';
+import MasterDepositCancel from '../../../components/UI/Modal/MasterDepositCancel/MasterDepositCancel';
+import MasterDepositeConfirm from '../../../components/UI/Modal/MasterDeposite/MasterDepositeConfirm';
+import axios from 'axios';
+import { PostMethod } from '../../../services/api-services';
+import Nodata from "../../../components/Nodata/Nodata"
+
+
+
+
+
+export default function MasterDepositeDataTable({data,num,setNum}) {
+
+   
+    const [ammount,setAmmount] = useState('');
+    const handleAmmount = (e)=>setAmmount(e.target.value);
+    const [id,setId] = React.useState("");
+    const [confirmOpen,setConfirmOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const confirmOpenHandler = ()=>setConfirmOpen(true);
+    const confirmCloseHandler = ()=>setConfirmOpen(false);
+    
+      const confirmHandler = async(id)=>{
+        try{
+        const response = await axios.request(
+          PostMethod(`api/affiliate-register-lists/accept/${id}`,
+          {user_type:"deposit-confirm"},
+        ));
+      
+        if(response.data.status="success"){
+          setNum(num+1);
+          toast.success(response.data.message);
+          return;
+        }
+        if(response.data.status="error"){
+          toast.error(response.data.message);
+          return;
+        }
+        }catch(error){
+          console.log(error)
+          toast.error(error.response.data.message)
+          console.log(error.response.data.message)
+          if (error.response.status === 401 || error.response.data.message === "Unauthenticated.") {
+          logoutHandler();
+          }
+          return;
+        }
+      }
+    
+      const columns = [
+        { field: 'id', headerName: "No", width: 90},
+        {
+          field: 'name',
+          headerName: 'Name',
+          width: 150,
+        },
+        {
+          field: 'payment_type',
+          headerName: 'Payment Type',
+          width: 200,
+          
+        },
+        {
+          field: 'payment_name',
+          headerName: 'Payment Name',
+          width: 200,
+       
+        },
+        {
+          field: 'transition_id',
+          headerName: 'Transition ID',
+          width: 150,
+        },
+        {
+          field: 'amount',
+          headerName: 'Amount',
+          width: 150,
+        },
+        { field:"date",
+          headerName: 'Date',
+          width: 200,
+          valueGetter:(param)=>ChangeDate(param.row.created_at)
+        },
+        { field:"action",
+          headerName: 'Action',
+          width: 250,
+          renderCell:(param)=>
+          <Stack spacing={1} direction="row">
+          <Button variant="contained" color="success" size="small" onClick={()=>{
+            confirmOpenHandler();setId(param.row.id)}
+            }>Confirm</Button>
+          <Button variant="outlined" onClick={()=>{handleOpen();setId(param.row.id);}} color="error" size="small">Reject</Button>
+         </Stack> 
+        },
+      ];
+
+  return (
+    <div style={{ height:"60vh",width: '100%' }}>
+     {
+         data.length !==0 ? <DataGrid
+         rows={data}
+         columns={columns}
+         pageSize={5}
+         rowsPerPageOptions={[5]}
+       />
+       :(
+        <Nodata />
+       )
+     }
+        <MasterDepositCancel open={open} handleClose={handleClose} setNum={setNum} id={id} num={num} handleAmmount={handleAmmount} />
+        <MasterDepositeConfirm open={confirmOpen} handleClose={confirmCloseHandler} setNum={setNum} num={num} submitHandler={confirmHandler} id={id} />
+    </div>
+  );
+}
