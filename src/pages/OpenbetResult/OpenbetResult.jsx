b1efbedc39345f6e60f83ffcf100d52fd005b6b7
Sai <sai081118@gmail.comgit commit>
2022-05-11 12:59:24 +0630

    betResult

--- src/pages/OpenbetResult/OpenbetResult.jsx
+++ src/pages/OpenbetResult/OpenbetResult.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./OpenbetResult.module.css";
 import { Grid } from "@mui/material";
 import ResultCard from "./ResultCard/ResultCard";
@@ -6,9 +6,10 @@ import PageTitleCard from "../../components/UI/PageTitleCard/PageTitleCard";
 import CustomGetFunction from "../../services/CustomGetFunction";
 import CustomLoading from "../../components/CustomLoading/CustomLoading";
 const OpenbetResult = () => {
+  const [render,setRender] =  useState(false);
   const { data, loading } = CustomGetFunction(
     "api/football-fixtures?status=active&source=backend",
-    []
+    [render]
   );
   return (
     <PageTitleCard title="Opening bet Result">
@@ -17,7 +18,7 @@ const OpenbetResult = () => {
           <Grid container rowSpacing={3}>
             {data.map((item, index) => (
                 <Grid item xs={4} key={index}>
-                  <ResultCard key={index} resultData={item} />
+                  <ResultCard key={index} resultData={item} render={render} setRender={setRender} />
                 </Grid>
               ))}
           </Grid>
