ec8a154a9006fddf3468e1230f41374ec31351c5
Sai <sai081118@gmail.comgit commit>
2022-05-14 11:17:45 +0630

    PartnerSite

--- src/pages/PartnerSite/AnswerQuestion/AnswerQuestion.jsx
+++ src/pages/PartnerSite/AnswerQuestion/AnswerQuestion.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import { Delete, Edit } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
+import React, { useState } from 'react'
+import AnswerQuestionModal from '../../../components/UI/Modal/AnswerQuestion/AnswerQuestionModal'
 import PageTitleCard from '../../../components/UI/PageTitleCard/PageTitleCard'
-import classes from "./AnswerQuestion"
+import classes from "./AnswerQuestion.module.css"
 
 const AnswerQuestion = () => {
-  return (
+  const [open,setOpen] = useState(false);
+  const handleOpen = ()=>setOpen(true);
+  const handleClose = ()=>setOpen(false)
+   return (
     <PageTitleCard title="Answer Question">
         <div className={classes["card-body"]}>
-
+        <div className={classes["form"]}>
+      <p>This is Question 1</p>
+     <IconButton>
+     <Edit onClick={handleOpen}/>
+     </IconButton>
+     <IconButton>
+     <Delete />
+     </IconButton>
         </div>
+        </div>
+        <AnswerQuestionModal open={open} handleClose={handleClose}/>
     </PageTitleCard>
   )
 }
--- src/pages/PartnerSite/ContactForm/ContactForm.jsx
+++ src/pages/PartnerSite/ContactForm/ContactForm.jsx
@@ -1,12 +1,44 @@
-import { TextField } from '@mui/material'
+
+import {TextField } from '@mui/material'
 import React, { useState } from 'react'
 import PageTitleCard from '../../../components/UI/PageTitleCard/PageTitleCard'
 import classes from "./ContactForm.module.css"
+import DemoDatePicker from './DemoDatePicker'
+import DemoTimePicker from './TimePicker'
+
 const ContactForm = () => {
     const [email,setEmail] = useState("");
   return (
    <PageTitleCard title="Contact Form">
        <div className={classes["card-body"]}>
+         <div className={classes["form"]}>
+           <label>Gmail</label>
+           <TextField size='small' sx={{width:300}}/>
+         </div>
+         <div className={classes["form"]}>
+           <label>Facebook</label>
+           <TextField size='small' sx={{width:300}}/>
+         </div>
+         <div className={classes["form"]}>
+           <label>Twitter</label>
+           <TextField size='small' sx={{width:300}}/>
+         </div>
+         <div className={classes["form"]}>
+           <label>Instagram</label>
+           <TextField size='small' sx={{width:300}}/>
+         </div>
+         <div className={classes["form"]}>
+           <label>Time</label>
+           <TextField size='small' sx={{width:300}}/>
+         </div>
+         {/* <div className={classes["form"]}>
+           <label>Date</label>
+           <DemoDatePicker/>
+         </div> */}
+         {/* <div className={classes["form"]}>
+           <label>Time</label>
+           <DemoTimePicker/>
+         </div> */}
         </div>
    </PageTitleCard>
   )
